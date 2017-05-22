3c0dd8685fb8a526aead3fe4587e755e4b54d477
ziggyiggy91 <up_isms@yahoo.com>
2017-05-21 20:31:20 -0700

    update_main

--- main.c
+++ main.c
@@ -16,37 +16,28 @@ void pitch_error(unsigned long);
 void init_meter(void);
 int i ;
 volatile uint32_t timer = 0,ready = 1,CaptureData = 0,average = 0,tick = 0,averager = 0,counter = 0;
-volatile  long error = 0;
+volatile  long error = 0, control_error = 0;
 volatile int lock = 1;
 volatile long Counts = 0; 
 volatile int shiftx = 5,shifty = 0;
 unsigned int string_select = 4; //starts off on string 4
 volatile static unsigned int pitch_select[] = {0,0,0,0}; //sets pitch to standard tuning
 volatile signed int pitch1 = 0, pitch2 = 0,pitch3 = 0,pitch4 = 0;
-unsigned int pitch_increase_flag; // increase pitch flag
-unsigned int pitch_decrease_flag; // decrease pitch flag
-unsigned long signal_time; //signal from circuit
 unsigned long signal_min; //minimum signal the string select will read
 unsigned long signal_max; //maximum signal the string select will read
-signed long signal_diff; // total difference between real and expected periods
 unsigned long pitch_index; //index for fixed signals
 unsigned int overflow; // checks if signal and string select are valid
 unsigned int timer_value;
 unsigned int count;
- 
+unsigned int duty = 10000;
+double kp = 0.1;
 const int sting4S = 7, sting3S = 7,sting2S = 7,sting1S = 7;
 char string4[sting4S][2] = {"E","D#","D","C#","C","B"}; 
 char string3[sting3S][2] = {"A","G#","G","F#","F","E"};
 char string2[sting2S][2] = {"D","C#","C","B","A#","A"};
 char string1[sting1S][2] = {"G","F#","F","E","D#","D"};
 
-//  B  F A# E 
-/*
-double string4P[] = {4100.0,3889.1,3670.7999999999993,3270.3,3086.7999999999997};
-double string3P[] = {5500.0,5191.299999999999,4899.9,4365.400000000001,4120.3};
-double string2P[] = {7341.5999999999985,6929.6,6540.6,6173.499999999999,5500.0};
-double string1P[] = {9798.999999999998,8730.7,8240.699999999999,7778.2,7341.5999999999985};
-*/
+ 
 //updated 4/24/17
 double string4P[] = {2439,2571.2,2724.2,2886.1,3057.8,3139.6};
 double string3P[] = {1818.18,1926.29,2040.8,2162.2,2290.7,2439.0};
@@ -62,19 +53,19 @@ double string1P[] = {1020.5,1081.09,1145.3,1213.4,1285.6449,1362.1};
 
 
 int i = 0;
- //System is running at 80Mhz to analyze with the logic analyzer
+ //System is running at 80Mhz  
 int main(){
 	PLL_Init();
 	UART0_Init();
 	portF_init();
 	EdgeCounter_Init();
-
+	PWM0A_Init(duty, 0);
+	PWM0B_Init(duty, 0);
 	ST7735_InitR(INITR_REDTAB);
 	ST7735_FillScreen(ST7735_BLACK);
 	ST7735_SetRotation(1);
   ST7735_DrawChar(0,0 ,'4', ST7735_GREEN , ST7735_BLACK, 8 );// Start off with tuning low E
  	ST7735_DrawChar(60,0 ,'E', ST7735_GREEN , ST7735_BLACK, 8);// Start off with tuning low E
-	//	ST7735_DrawLine(10+shiftx,  90+shifty, 136+shiftx, 90+shifty, ST7735_YELLOW);
  	
   SysTick_Init(800);	 //80MHz sampling at .01ms
  	EnableInterrupts();          
@@ -109,7 +100,7 @@ switch(string_select){
 				case 4:
 					pitch4 -= 1;
  					pitch4 = (pitch4+6) % 6;
- 				 UART_OutUDec(abs(pitch4));
+ 				  UART_OutUDec(abs(pitch4));
 					UART_OutChar(CR);
 					UART_OutChar(LF);
 					pitch_Handler(string_select,abs(pitch4));
@@ -160,6 +151,7 @@ void GPIOPortD_Handler(){ 		GPIO_PORTF_DATA_R = 0x02;
 					 error = string1P[pitch1]-Counts;
 					 break;
 				 }
+				control_error  = error*kp; //Proportional Control System
  				UART_OutUDec(1/(Counts*.0000001));
 				UART_OutChar('H');
 				UART_OutChar('Z');
@@ -179,18 +171,22 @@ void GPIOPortD_Handler(){ 		GPIO_PORTF_DATA_R = 0x02;
        break;
    case 0x02:									//E-String
 		 	string_select = 4;
+	    signal_max = string4P[0]; signal_min = string4P[5];
  	    pitch_Handler(4,pitch4);
  			break;
 		case 0x04:								//A-String
 			string_select = 3;
+		  signal_max = string3P[0]; signal_min = string3P[5];
  			pitch_Handler(3,pitch3);
  			break;
 		case 0x40:								//D-String
 			string_select = 2;
+		  signal_max = string2P[0]; signal_min = string2P[5];
  			pitch_Handler(2,pitch2);
  			break;
 		case 0x08:								//G-String
 			string_select = 1;
+			signal_max = string1P[0]; signal_min = string1P[5];
       pitch_Handler(1,pitch1);
  			break;
 		case 0x80:
@@ -216,9 +212,6 @@ void GPIOPortD_Handler(){ 		GPIO_PORTF_DATA_R = 0x02;
 					pitch_Handler(string_select,abs(pitch4));
 				break;
 			}
-   		/*UART_OutChar('+');
-			UART_OutChar(CR);
-			UART_OutChar(LF);*/
  			break;
   }
 				GPIO_PORTD_ICR_R = 0xFF;
@@ -254,32 +247,25 @@ void pitch_Handler(uint8_t string_select,uint8_t pitch_select){
 	
  //Graphical layout for the ST7735
 void pitch_error(unsigned long error){
-	/*
- 	if(error>-1219.5 && error <-200){}
-	else if(error>-200 && error <-150){}
-	else if(error>-100&& error <-50){}
-	else 
-	*/if((error>(0) && error <60)&&lock!=1){
+	if((error>(0) && error <60)&&lock!=1){
 			GPIO_PORTF_DATA_R = 0x08;
-init_meter();
-  ST7735_FillRect(10+(12.5*5), 80+shifty, 14, 10, ST7735_GREEN);
-		lock = 1;
-	}/*
-	else if(error>0 && error <50){}
-		else if(error>50 && error <100){}
-			else if(error>100 && error <150){}*/
-				else if((error>60 && error <2000)&&lock!= 1){
-					init_meter();
-					  ST7735_FillRect(10+(12.5*6), 80+shifty, 14, 10, ST7735_RED);
-					lock = 1;
+			init_meter();
+			ST7735_FillRect(10+(12.5*5), 80+shifty, 14, 10, ST7735_GREEN);
+			lock = 1;
+	 }
+	   else if((error>60 && error <2000)&&lock!= 1){
+		   init_meter();
+			 ST7735_FillRect(10+(12.5*6), 80+shifty, 14, 10, ST7735_RED);
+			 PWM0A_Duty((abs(control_error)/signal_min) * duty);
+
+		   lock = 1;
 				}
 				 else if((error <2000000)&&lock!= 1){
 					init_meter();
-					  ST7735_FillRect(10+(12.5*4), 80+shifty, 14, 10, ST7735_RED);
+					ST7735_FillRect(10+(12.5*4), 80+shifty, 14, 10, ST7735_RED);
+					PWM0B_Duty((abs(control_error)/signal_min) * duty);
 					lock = 1;
 				}
- 
-
   }
 
  void init_meter(){
